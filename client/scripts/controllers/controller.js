76caecee0666359de8b6f52a801d044f4b553017
paulstafki <waynesworldwails@hotmail.com>
2015-10-05 18:18:04 -0500

    Added bottom Nav links

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -1,9 +1,5 @@
 myApp.controller('SomeController', ["$scope", function($scope){
-    $scope.thing = "Here you go!";
-    console.log("Some Controller");
 }]);
 
 myApp.controller('AnotherController', ["$scope", function($scope){
-    $scope.thing = "There you are!";
-    console.log("Another Controller");
 }]);
