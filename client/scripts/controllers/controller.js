b97794524badb782952cd767d2e613096d48f492
paulstafki <waynesworldwails@hotmail.com>
2015-10-15 17:28:23 -0500

    Controllers altered

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -1,5 +1,7 @@
 myApp.controller('SomeController', ["$scope", function($scope){
+    console.log("Whatcha looking for in here?!");
 }]);
 
 myApp.controller('AnotherController', ["$scope", function($scope){
+    console.log("Nope, nothing fun here either.");
 }]);
